fc1f7aa76dbda7c92d6d9f6175158c6834c2f1c3
Andre Oliveira <desmonplays@gmail.com>
2020-11-15 14:15:57 -0300

    feat: oneline and infinite calc

--- src/calc.c
+++ src/calc.c
@@ -5,34 +5,31 @@ int main() {
   float v1, v2, result;
   char op;
 
-  printf("type the first number: ");
-  scanf("%f", &v1);
+  while (1) {
+    printf("type the calc: ");
+    scanf("%f %c %f", &v1, &op, &v2);
 
-  printf("type the operation: ");
-  scanf(" %c", &op);
-
-  printf("type the second number: ");
-  scanf("%f", &v2);
-
-  switch(op) {
-    case '+':
-      result = v1 + v2;
-      break;
-    case '-':
-      result = v1 - v2;
-      break;
-    case '*':
-      result = v1 * v2;
-      break;
-    case '/':
-      result = v1 / v2;
-      break;
-    default:
-      result = 0;
-      printf("invalid\n");
-      break;
+    switch(op) {
+      case '+':
+        result = v1 + v2;
+        break;
+      case '-':
+        result = v1 - v2;
+        break;
+      case '*':
+        result = v1 * v2;
+        break;
+      case '/':
+        result = v1 / v2;
+        break;
+      default:
+        result = 0;
+        printf("invalid\n");
+        break;
+    }
+    
+    printf("the result: %f\n", result);
   }
 
-  printf("the result: %f\n", result);
   return 0;
 }
