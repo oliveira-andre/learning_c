0f1362f4aecd4469feaec45ab426c025565fe631
Andre Oliveira <desmonplays@gmail.com>
2020-11-15 15:40:44 -0300

    feat: switch calc to a function operation

--- src/calc.c
+++ src/calc.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float calc(float v1, char op, float v2) {
+  float result;
+
+  switch(op) {
+    case '+':
+      result = v1 + v2;
+      break;
+    case '-':
+      result = v1 - v2;
+      break;
+    case '*':
+      result = v1 * v2;
+      break;
+    case '/':
+      result = v1 / v2;
+      break;
+    default:
+      result = 0;
+      printf("invalid\n");
+      break;
+  }
+
+  return result;
+}
+
 int main() {
   float v1, v2, result;
   char op;
@@ -9,25 +34,7 @@ int main() {
     printf("type the calc: ");
     scanf("%f %c %f", &v1, &op, &v2);
 
-    switch(op) {
-      case '+':
-        result = v1 + v2;
-        break;
-      case '-':
-        result = v1 - v2;
-        break;
-      case '*':
-        result = v1 * v2;
-        break;
-      case '/':
-        result = v1 / v2;
-        break;
-      default:
-        result = 0;
-        printf("invalid\n");
-        break;
-    }
-    
+    result = calc(v1, op, v2);
     printf("the result: %f\n", result);
   }
 
