16b701dafa9a2148c47f6e7d57cb5de52d372087
Andre Oliveira <desmonplays@gmail.com>
2020-11-22 16:04:54 -0300

    feat: validating coordinates on hash game

--- src/hash_game.c
+++ src/hash_game.c
@@ -20,17 +20,37 @@ void showBoard(char board[3][3]) {
     for (j = 0; j < 3; j++) {
       printf("%c |", board[i][j]);
     }
+
     printf("\n");
   }
+
+  printf("\n\n");
 }
 
 int main() {
   char board[3][3];
   char c1 = 'X';
   char c2 = 'O';
+  int x, y;
 
   resetBoard(board);
   showBoard(board);
+
+  while (1) {
+    printf("Type the coordinates: ");
+    scanf("%d %d", &x, &y);
+
+
+    if (board[x][y] == ' ') {
+      board[x][y] = c1;
+    } else if ((x > 2 || y > 2) || (x < 0 || y < 0)) {
+      printf("invalid coordinates\n");
+    } else {
+      printf("this position cannot be used\n");
+    }
+
+    showBoard(board);
+  }
 
   return 0;
 }
